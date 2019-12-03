92486ac390e4d583c825368bbb4ee2033f66da95
Jacob Kenny <jacobkenny05@gmail.com>
2019-12-03 17:38:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+
+def map(array)
+  my_array = []
+  i = 0 
+  while i < array.length do 
+    my_array.push(yield(array[i]))
+    i += 1 
+  end
+  my_array 
+end  
+
+def reduce(array, starting_value = nil)
+   if starting_value  
+    running_total = starting_value
+    i = 0 
+  else 
+    running_total = array[0]
+    i = 1 
+  end   
+  while i < array.length
+   running_total = yield(running_total, array[i])
+    i += 1 
+    #binding.pry 
+  end 
+  running_total
+  
+end   
+  
+  
