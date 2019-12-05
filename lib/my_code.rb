bae1beba66105a15d1fbe39edeb46cef600931dd
Daniel Liu <danliu277@gmail.com>
2019-12-05 00:17:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(array)
+  index = 0
+  result = []
+  while index < array.length do
+    result.push(yield(array[index]))
+    index += 1
+  end
+  return result
+end
+
+def reduce(array, start = nil)
+  index = 0
+  if(start)
+    result = start
+  else
+    result = array[0]
+    index = 1
+  end
+  while index < array.length do
+    result = yield(result, array[index])
+    index += 1
+  end
+  pp result
+  return result
+end
