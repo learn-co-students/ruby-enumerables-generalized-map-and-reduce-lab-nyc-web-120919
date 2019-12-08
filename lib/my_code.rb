ccf8833c46e70c47761e6d1ab7d5497ca558bc98
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 01:56:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(arr)
+  result = []
+  i = 0
+  while i < arr.length do
+    result.push(yield(arr[i]))
+    i += 1
+  end
+  result
+end
+
+def reduce(arr, start = nil)
+  if start
+    total = start
+    i = 0
+  else
+    total = arr[0]
+    i = 1
+  end
+  while i < arr.length do
+    total = yield(total, arr[i])
+    i += 1
+  end
+  total
+end
